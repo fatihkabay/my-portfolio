d60758f3c0f65af5bfd73fdbe16928bb7d6f9cae
Fatih Kabay <mfthkabay@gmail.com>
2024-02-04 19:01:20 +0300

    icon link and sidebar design

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faCodepen, faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -23,6 +24,28 @@ const Sidebar = () => (
           <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
         </NavLink>
     </nav>
+    <ul>
+        <li>
+            <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/fatih-kabay-b0b7a7229/'>
+                <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e' />
+            </a>
+        </li>
+        <li>
+            <a target='_blank' rel='noreferrer' href='https://github.com/fatihkabay'>
+                <FontAwesomeIcon icon={faGithub} color='#4d4d4e' />
+            </a>
+        </li>
+        <li>
+            <a target='_blank' rel='noreferrer' href='https://www.instagram.com/the_fatihh0/'>
+                <FontAwesomeIcon icon={faInstagram} color='#4d4d4e' />
+            </a>
+        </li>
+        <li>
+            <a target='_blank' rel='noreferrer' href='https://codepen.io/Fatih-Kabay'>
+                <FontAwesomeIcon icon={faCodepen} color='#4d4d4e' />
+            </a>
+        </li>
+    </ul>
   </div>
 )
 export default Sidebar
