22e3f83a42d2ac0de6cd5907026b709073f353eb
Fatih Kabay <mfthkabay@gmail.com>
2024-02-14 20:36:02 +0300

    building home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,22 +1,62 @@
-import LogoTitle from "../../assets/images/logo-s.png";
-import "./index.scss";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import LogoTitle from '../../assets/images/logo-s.png'
+import './index.scss'
+import { Link } from 'react-router-dom'
+import AnimatedLetter from '../AnimatedLetters'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['a', 'b', 'a', 'y']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    '',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+  ]
+
+  useEffect(() => {
+     setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi! <br /> I'm
-          <img src={LogoTitle} alt="developer" />
-          kabay
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
           <br />
-          web developer
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetter
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetter
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
         </h1>
-        <h2>Full Stack Developer / Javascript Expert / Studying In High School</h2>
+        <h2>
+          Full Stack Developer / Javascript Expert / Studying In High School
+        </h2>
         <Link to="/contact" className="flat-button">
-            CONTACT ME
-          </Link>
+          CONTACT ME
+        </Link>
       </div>
     </div>
   )
