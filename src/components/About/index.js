2ef1fbc7eb88b9ecd94b4aa9e026abf5b8153faf
Fatih Kabay <mfthkabay@gmail.com>
2024-02-18 19:38:30 +0300

    build about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters/index'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngular, faCss3, faGithub, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -33,6 +35,28 @@ const About = () => {
           I have visual studio usage, frontend and backend certification. I have
           a series on duolingo for more than 1 year.
         </p>
+      </div>
+      <div className='stage-cube-cont'>
+        <div className='cubespinner'>
+           <div className='face1'>
+            <FontAwesomeIcon icon={faAngular} color='#DD0031' />
+           </div>
+           <div className='face2'>
+            <FontAwesomeIcon icon={faHtml5} color='#F06529' />
+           </div>
+           <div className='face3'>
+            <FontAwesomeIcon icon={faCss3} color='#28A4D9' />
+           </div>
+           <div className='face4'>
+            <FontAwesomeIcon icon={faReact} color='#5ED4F4' />
+           </div>
+           <div className='face5'>
+            <FontAwesomeIcon icon={faJsSquare} color='#EFD81D' />
+           </div>
+           <div className='face6'>
+            <FontAwesomeIcon icon={faGithub} color='#212121' />
+           </div>
+        </div>
       </div>
     </div>
   )
