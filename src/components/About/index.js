1caadffa3c40439cb5f59a4b8caed4f156cf50af
Fatih Kabay <mfthkabay@gmail.com>
2024-02-19 23:13:51 +0300

    loader

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,7 +2,15 @@ import React, { useState, useEffect } from 'react'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters/index'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngular, faCss3, faGithub, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
+import {
+  faAngular,
+  faCss3,
+  faGithub,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -13,52 +21,55 @@ const About = () => {
     }, 4000)
   }, [])
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={['A', 'b', 'o', 'u', 't', '', 'm', 'e']}
-            idx={15}
-          />
-        </h1>
-        <p>
-          My name is Mehmet Fatih and my surname is Kabay. I am 16 years old. I
-          am studying in high school. I develop front-end and back-end, I have
-          accumulated database knowledge.
-        </p>
-        <p>
-          I would like to work as a full stack developer. I have B1 level
-          English. I'm currently doing an internship somewhere
-        </p>
-        <p>
-          I have visual studio usage, frontend and backend certification. I have
-          a series on duolingo for more than 1 year.
-        </p>
-      </div>
-      <div className='stage-cube-cont'>
-        <div className='cubespinner'>
-           <div className='face1'>
-            <FontAwesomeIcon icon={faAngular} color='#DD0031' />
-           </div>
-           <div className='face2'>
-            <FontAwesomeIcon icon={faHtml5} color='#F06529' />
-           </div>
-           <div className='face3'>
-            <FontAwesomeIcon icon={faCss3} color='#28A4D9' />
-           </div>
-           <div className='face4'>
-            <FontAwesomeIcon icon={faReact} color='#5ED4F4' />
-           </div>
-           <div className='face5'>
-            <FontAwesomeIcon icon={faJsSquare} color='#EFD81D' />
-           </div>
-           <div className='face6'>
-            <FontAwesomeIcon icon={faGithub} color='#212121' />
-           </div>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', '', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            My name is Mehmet Fatih and my surname is Kabay. I am 16 years old.
+            I am studying in high school. I develop front-end and back-end, I
+            have accumulated database knowledge.
+          </p>
+          <p>
+            I would like to work as a full stack developer. I have B1 level
+            English. I'm currently doing an internship somewhere
+          </p>
+          <p>
+            I have visual studio usage, frontend and backend certification. I
+            have a series on duolingo for more than 1 year.
+          </p>
+        </div>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGithub} color="#212121" />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <Loader type="ball-clip-rotate-multiple" />
+    </>
   )
 }
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import './index.scss'
 import { Link } from 'react-router-dom'
 import AnimatedLetter from '../AnimatedLetters'
 import Logo from './Logo'
+import Loader from 'react-loaders'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -31,36 +32,39 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          <img src={LogoTitle} alt="developer" />
-          <AnimatedLetter
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetter
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img src={LogoTitle} alt="developer" />
+            <AnimatedLetter
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetter
               letterClass={letterClass}
               strArray={jobArray}
               idx={22}
             />
-        </h1>
-        <h2>
-          Full Stack Developer / Javascript Expert / Studying In High School
-        </h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+          </h1>
+          <h2>
+            Full Stack Developer / Javascript Expert / Studying In High School
+          </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type="ball-clip-rotate-multiple" />
+    </>
   )
 }
 
